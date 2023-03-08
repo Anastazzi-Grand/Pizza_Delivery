f77609490c2e6ce1b0a9f99b63967e01469ad032
akresnikov <kvwi7rYA>
2023-03-08 23:23:15 +0300

    Реализация мок сервиса

--- pizza.client/src/App.vue
+++ pizza.client/src/App.vue
@@ -6,13 +6,16 @@
       <Main class="col-8"/>
     </div>
   </div>
-  
+
 </template>
 
 <script>
 import Header from './layout/HeaderComponent.vue';
 import Main from './layout/MainComponent.vue';
 import Aside from './layout/AsideComponent.vue';
+import { config } from '@/config';
+import { MockDataService } from '@/services/data.service/mock.data.service';
+import { DataService } from '@/services/data.service/data.service';
 
 
 export default {
@@ -21,7 +24,12 @@ export default {
     Header,
     Main,
     Aside
-}
+  },
+  provide() {
+    return {
+      dataService: config.demo ? new MockDataService() : new DataService()
+    }
+  }
 }
 </script>
 
--- pizza.client/src/components/ProductsCard.vue
+++ pizza.client/src/components/ProductsCard.vue
@@ -29,7 +29,7 @@ export default {
                     }[];
             price: number;
             image: string;
-            coockingTime: string
+            cookingTime: string
         }
         */
         product: Object
