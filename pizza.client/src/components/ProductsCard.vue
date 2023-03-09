323afa0b564c2c82b9131d73dfc71c28c2953460
Nastya <vnedostupa@yandex.ru>
2023-03-09 11:00:22 +0300

    Корректирование вывода суммы на карточке

--- pizza.client/src/components/ProductsCard.vue
+++ pizza.client/src/components/ProductsCard.vue
@@ -1,8 +1,4 @@
 <template>
-    <div>
-        {{ product.name }}
-        цена: {{ product.price }}
-    </div>
     <div class="card" style="width: 18rem;">
         <img :src="product.image" class="card-img-top" :alt="product.name">
         <div class="card-body">
@@ -14,14 +10,14 @@
             <div v-for="(option, index) in property.options" v-bind:key="option.key" class="form-check">
                 <input class="form-check-input" type="radio" :checked="index===0" :name="property.name+product.name" :id="option.key+'-'+product.id">
                 <label class="form-check-label" :for="option.key+'-'+product.id">
-                  {{ option.value }} {{ option.markUp }} руб.
+                  {{ option.value }} {{ (option.markUp + product.price)}} руб.
                 </label>
               </div>
           </div>
           <a href="#" class="btn btn-primary" @click="basket.addProductToBasket(product)">Заказать</a>
           <div class="btn-group" role="group" aria-label="Basic example">
             <button type="button" class="btn btn-primary">-</button>
-            <button type="button" class="btn btn-primary">Middle</button>
+            <button type="button" class="btn btn-primary">Выбрать</button>
             <button type="button" class="btn btn-primary">+</button>
           </div>
         </div>
